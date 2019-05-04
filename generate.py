f8140f3d80febb81242d1236256142dafec89b41
Jorge Luis Rangel Garcia <jorgeluisrangelgarcia@MacBook-Air-de-Jorge.local>
2019-05-03 19:48:29 -0500

    Starting git repo

---
+++ generate.py
@@ -0,0 +1,12 @@
+import os
+
+markdowns = os.listdir('markdown')
+print("Markdown files to convert: "+str(len(markdowns)))
+os.system('rm -rf html-content/posts/')
+os.makedirs('html-content/posts')
+
+for m in markdowns:
+    os.system('pandoc markdown/'+m+' -o html-content/posts/'+m[:-3]+'.html')
+
+posts = os.listdir('html-content/posts')
+print('Html files converted: '+str(len(posts)))
