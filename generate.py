2dbb5ca5f983eed84f4163d24451867ffc9f4c36
Jorge Luis Rangel Garcia <jorgeluisrangelgarcia@MacBook-Air-de-Jorge.local>
2019-05-04 00:00:42 -0500

    Added markdown directory

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ os.system('rm -rf html-content/posts/')
 os.makedirs('html-content/posts')
 
 for m in markdowns:
-    os.system('pandoc markdown/'+m+' -o html-content/posts/'+m[:-3]+'.html')
+    os.system('pandoc markdown/'+m+' -o html-content/posts/'+m[:-8]+'.html')
 
 posts = os.listdir('html-content/posts')
 print('Html files converted: '+str(len(posts)))
